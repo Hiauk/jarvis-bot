5f958a61f4996725a31d2896ca401c5d912a00c4
KU54795 <t.raymond@kingston.ac.uk>
2019-01-10 09:22:26 +0000

    - Added @staticmethod decorators to IgnoreList.py so my linter stops complaining

--- IgnoreList.py
+++ IgnoreList.py
@@ -10,34 +10,41 @@ class IgnoreList:
     def __init__(self, roomID, ignoredUsers):
         self.roomID = roomID
         self.ignoredUsers = ignoredUsers
+
+    @staticmethod
     def LoadIgnoreList():
         unpicklefile = open(os.path.join(script_dir, ignoreListName + '.pkl'), 'rb')
         IgnoreList.GlobalIgnoreLists = pickle.load(unpicklefile)
         unpicklefile.close()
 
+    @staticmethod
     def SaveIgnoreList():
         file = open(os.path.join(script_dir, ignoreListName + '.pkl'), 'wb')
         pickle.dump(IgnoreList.GlobalIgnoreLists,file)
         file.close()
 
+    @staticmethod
     def AddNewRoom(roomID):
         newRoom = IgnoreList(roomID, [])
         IgnoreList.GlobalIgnoreLists.append(newRoom)
         IgnoreList.SaveIgnoreList()
         return newRoom
 
+    @staticmethod
     def AddUser(roomID, userID):
         for anIgnoreList in IgnoreList.GlobalIgnoreLists:
             if anIgnoreList.roomID == roomID:
                 anIgnoreList.ignoredUsers.append(userID)
         IgnoreList.SaveIgnoreList()
 
+    @staticmethod
     def RemoveUser(roomID, userID):
         for anIgnoreList in IgnoreList.GlobalIgnoreLists:
             if anIgnoreList.roomID == roomID:
                 anIgnoreList.ignoredUsers.remove(userID)
         IgnoreList.SaveIgnoreList()
 
+    @staticmethod
     def GetGlobalIgnoreList():
         return IgnoreList.GlobalIgnoreLists
 
