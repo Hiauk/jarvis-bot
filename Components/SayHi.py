5f763914a0fd05e8c26ac169c9e4ecae1ce3a2c8
Daniel Ball <danieljosephball@icloud.com>
2019-02-14 19:46:15 +0000

    Add space to Hi check

--- Components/SayHi.py
+++ Components/SayHi.py
@@ -6,6 +6,6 @@ class HiResponse(Component):
         Component.__init__(self, self)
     
     def OnMessageReceived(self, room, event):
-        if re.search("Hi", event['content']['body']):
+        if re.search("Hi ", event['content']['body']):
             # Somebody said hi, let's say Hi back
             room.send_text("Hi, " + event['sender'])
