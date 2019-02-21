51d4157a7f645d4e3e1d2726c6deb6a8346f907c
Daniel Ball <danieljosephball@icloud.com>
2019-02-21 16:15:15 +0000

    Parse to string

--- Components/Roll.py
+++ Components/Roll.py
@@ -11,7 +11,7 @@ class Roll(Component):
         dice = args[1]
         info = dice.split("d")
         for x in range(int(info[0])):
-            room.send_text(random.randint(1,int(info[1]) + 1))
+            room.send_text(str(random.randint(1,int(info[1]) + 1)))
 
     def OnCommandReceived(self, room, event):
         args = event['content']['body'].split()
